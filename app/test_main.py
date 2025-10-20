1d3347b493158969b11c996295c0d9f91a50371a
Dawid <kucharzjaw@gmail.com>
2025-10-20 15:55:59 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("Pass@word1", True),
+        ("Dawid12345#", True),
+
+        ("Daw@1", False),  # to short
+        ("password@1", False),  # brak duzych liter, brak cyfry, brak znaku specjalnego
+        ("A" * 17 + "1@", False),  # za dlugie
+        ("Password1", False),
+        ("Password#", False),
+        ("Pass word#", False),
+    ],
+)
+def test_check_password(password, expected):
+    result = check_password(password)
+    assert result == expected
